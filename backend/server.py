70bf11e328b94d23efdb98e5deb1382c56b01e65
DaivikModh <daivikmodh9@gmail.com>
2026-09-24 19:30:50 +0530

    initial commit project setup

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api/test", methods=["GET"])
+def test():
+    return jsonify({
+        "message": "Hello from Flask"
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
